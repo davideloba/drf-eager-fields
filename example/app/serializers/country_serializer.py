3f80c782265744df97f7c3bde9ae0a8c501c02fd
Davide Lo Bartolo <davide.lobartolo@vedrai.com>
2022-04-15 15:15:01 +0000

    missing "Meta" test and example added

--- example/app/serializers/country_serializer.py
+++ example/app/serializers/country_serializer.py
@@ -1,7 +1,9 @@
 from django.db.models.query import Prefetch
 from django.utils.functional import classproperty
-from drf_eager_fields.serializers import EagerFieldsSerializer
 from rest_framework import serializers
+from drf_eager_fields.serializers import EagerFieldsSerializer
+
+from ..serializers.customer_serializer import LazyCustomerSerializer
 
 from ..models import Country
 
@@ -26,8 +28,16 @@ class CountrySerializer(serializers.ModelSerializer, EagerFieldsSerializer):
                 },
                 "region": {
                     "field": RegionSerializer(),
-                    "prefetch": Prefetch(
-                        "region", queryset=RegionSerializer.Meta.model.objects.all()
-                    ),
+                    "prefetch": Prefetch("region", queryset=RegionSerializer.Meta.model.objects.all()),
                 },
             }
+
+
+class MixedCountrySerializer(serializers.ModelSerializer, EagerFieldsSerializer):
+    """EagerFieldsSerializer without the 'extra' field, for testing purposes"""
+
+    customers = LazyCustomerSerializer(many=True)
+
+    class Meta:
+        fields = ("id", "name", "customers")
+        model = Country
